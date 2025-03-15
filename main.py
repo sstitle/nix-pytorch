1218aa69d74c10632abb5afac9d7077865c336b9
Sam Title <samstitle@gmail.com>
2025-03-15 17:43:36 -0400

    Baby's first tensor

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World from Python")
+import torch
+
+# Example usage of PyTorch: Creating a tensor
+tensor = torch.tensor([1, 2, 3, 4, 5])
+print("Hello World from PyTorch")
+print("Example tensor:", tensor)
